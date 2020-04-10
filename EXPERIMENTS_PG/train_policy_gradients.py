d334551ac9cf36760d232288c31406a46d7dfb80
Robert Lange <robert.lange@barcelonagse.eu>
2020-04-10 12:44:55 +0200

    Add DDPG Shell

--- EXPERIMENTS_PG/train_policy_gradients.py
+++ EXPERIMENTS_PG/train_policy_gradients.py
@@ -4,10 +4,10 @@ import torch
 from drl_toolbox.utils import DotDic
 from drl_toolbox.utils import set_random_seeds, load_config
 from drl_toolbox.dl import BodyBuilder, RecurrentBodyBuilder
-from drl_toolbox.dl import set_optimizer
+from drl_toolbox.dl import set_optimizer, DeepLogger
 
-from drl_toolbox.single_rl import make_parallel_env
-from drl_toolbox.single_rl.agents import VPG_Agent
+from drl_toolbox.single_rl import make_parallel_env, ActorCriticBuilder
+from drl_toolbox.single_rl.agents import VPG_Agent, A2C_Agent, DDPG_Agent
 
 
 def main(train_config, net_config, log_config):
@@ -17,23 +17,51 @@ def main(train_config, net_config, log_config):
     device = torch.device(device_name)
     set_random_seeds(seed_id=train_config.seed_id, verbose=False)
 
-    # Define the DQN network architecture & optimizer
-    policy_network = BodyBuilder(**net_config).to(device)
-    optimizer = set_optimizer(network=policy_network,
-                              opt_type=train_config.opt_type,
-                              l_rate=train_config.l_rate)
-
     # Agent - Envs, Nets, Optimizer, Train Config, Logger
     train_log = DeepLogger(**log_config)
-    train_env = make_parallel_env(train_config.env_name,
-                                  train_config.num_train_threads)
-    test_env = make_parallel_env(train_config.env_name,
-                                 train_config.num_test_threads)
+    env = make_parallel_env(train_config.env_name,
+                            train_config.seed_id,
+                            train_config.num_threads)
 
-    pg_agent = VPG_Agent(train_env, test_env, train_config, train_log,
-                         policy_network, optimizer, device)
+    if train_config.agent_name == "VPG":
+        # Define the policy network architecture & optimizer
+        policy_network = BodyBuilder(**net_config).to(device)
+        optimizer = set_optimizer(network=policy_network,
+                                  opt_type=train_config.opt_type,
+                                  l_rate=train_config.l_rate)
 
-    # Run the training of the DQN agent
+        # Instantiate PG agent class with both the actor net (use full returns)
+        pg_agent = VPG_Agent(env, train_config, train_log,
+                             policy_network, optimizer, device)
+
+    elif train_config.agent_name == "A2C":
+        # Define the actor-critic network architecture & optimizer
+        actor_critic_network = ActorCriticBuilder(**net_config["value"]).to(device)
+        optimizer = set_optimizer(network=actor_critic_network,
+                                  opt_type=train_config.opt_type,
+                                  l_rate=train_config.l_rate)
+
+        # Instantiate A2C agent class with both the actor & critic networks
+        pg_agent = A2C_Agent(env, train_config, train_log,
+                             actor_critic_network, optimizer, device)
+
+    elif train_config.agent_name == "DDPG":
+        # Define the actor-critic network architecture & optimizer
+        actor_critic_network = ActorCriticBuilder(**net_config["value"]).to(device)
+        value_optimizer = set_optimizer(network=actor_critic_network,
+                                        opt_type=train_config.opt_type,
+                                        l_rate=train_config.l_rate)
+
+        # Define the target networks
+
+        # Define the memory buffer system
+
+        # Define the exploration strategy of DDPG Agent - OU process
+
+        # Instantiate DDPG agent class w. networks/targets/buffer/exploration
+        pg_agent = DDPG_Agent()
+
+    # Run the training loop for the Policy Gradient agent
     pg_agent.run_learning_loop(num_train_updates=train_config.num_train_updates)
     return train_log, policy_network
 
@@ -49,7 +77,7 @@ if __name__ == "__main__":
 
     cmd_args = get_cmd_args()
     config = load_config(cmd_args.config_fname)
-    # config.log_config.config_fname = cmd_args.config_fname
+    config.log_config.config_fname = cmd_args.config_fname
 
     net_config = DotDic(config["net_config"])
     train_config = DotDic(config["train_config"])
